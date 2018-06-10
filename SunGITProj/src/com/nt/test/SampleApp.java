9e50313062862cf87a007a7f3c19814d5c765469
janiDev1 <janiDev1@gmail.com>
2018-06-10 10:46:30 +0530

    Delete chanage-->TL

--- SunGITProj/src/com/nt/test/SampleApp.java
+++ SunGITProj/src/com/nt/test/SampleApp.java
@@ -1,5 +1,5 @@
 package com.nt.test;
-import com.nt.test.Math;
+
 public class SampleApp {
 
 	public static void main(String[] args) {
@@ -7,6 +7,6 @@ public class SampleApp {
 	    for(int i=1;i<=10;++i){
 	    	System.out.println(i);
 	    }
-	    System.out.println("add==="+new Math().sum(100,200));
+	  /*  System.out.println("add==="+new Math().sum(100,200));*/
 	}//main
 }//class
