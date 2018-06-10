c2040f8f7af5ab731f173b5d9f59f1d260ad057c
janiDev1 <janiDev1@gmail.com>
2018-06-10 10:30:13 +0530

    Chanage1-->Dev2

--- SunGITProj/src/com/nt/test/SampleApp.java
+++ SunGITProj/src/com/nt/test/SampleApp.java
@@ -4,6 +4,8 @@ public class SampleApp {
 
 	public static void main(String[] args) {
 	    System.out.println("hello");
-	}
-
-}
+	    for(int i=1;i<=10;++i){
+	    	System.out.println(i);
+	    }
+	}//main
+}//class
